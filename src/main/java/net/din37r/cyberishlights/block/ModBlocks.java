d9f553b4206fe59754653f698587d61766e6bbd1
Din37r <dinarzekerieec@gmail.com>
2026-05-12 22:19:26 +0500

    Replace NeonLampBlock class with vanilla RedstoneLampBlock class. Remove the NeonLampBlock class.

--- src/main/java/net/din37r/cyberishlights/block/ModBlocks.java
+++ src/main/java/net/din37r/cyberishlights/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RedstoneLampBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -22,82 +23,82 @@ public class ModBlocks {
 
     //Blocks here
     public static final RegistryObject<Block> WHITE_NEON_LAMP = registerBlock("white_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> LIGHT_GRAY_NEON_LAMP = registerBlock("light_gray_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> GRAY_NEON_LAMP = registerBlock("gray_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> BLACK_NEON_LAMP = registerBlock("black_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> BROWN_NEON_LAMP = registerBlock("brown_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> RED_NEON_LAMP = registerBlock("red_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> ORANGE_NEON_LAMP = registerBlock("orange_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> YELLOW_NEON_LAMP = registerBlock("yellow_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> LIME_NEON_LAMP = registerBlock("lime_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> GREEN_NEON_LAMP = registerBlock("green_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> CYAN_NEON_LAMP = registerBlock("cyan_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> LIGHT_BLUE_NEON_LAMP = registerBlock("light_blue_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> BLUE_NEON_LAMP = registerBlock("blue_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> PURPLE_NEON_LAMP = registerBlock("purple_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> MAGENTA_NEON_LAMP = registerBlock("magenta_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
     public static final RegistryObject<Block> PINK_NEON_LAMP = registerBlock("pink_neon_lamp",
-            () -> new NeonLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
+            () -> new RedstoneLampBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP)
                     .strength(0.8F)
                     .sound(SoundType.LANTERN)
                     .lightLevel(state -> state.getValue(BlockStateProperties.LIT) ? 15 : 0)));
